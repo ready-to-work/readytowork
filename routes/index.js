a7cd13f4eb020cf251a7153a0f0635d297708435
Kelvin <kelvin.z.pan@gmail.com>
2018-02-15 16:52:30 -0800

    Tweaks to dummy data

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-
 /*
  * GET login page.
  */
 
+// Pass in login dummy data
+var logins = require('../dummy_data/logins.json');
+
 exports.view = function(req, res){
-  res.render('index');
+	res.render('index', logins);
 };
