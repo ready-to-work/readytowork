f4a73f3942aed6701159baff0b314e484294b3e4
Kelvin <kelvin.z.pan@gmail.com>
2018-02-15 21:46:18 -0800

    Completed login styling

--- public/js/index.js
+++ public/js/index.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
 function userLoginAuth(form)
 {
-	if(form.userid.value == "tester" && form.pass.value == "1234")
+	if(form.userid.value == "tester" && form.pass.value == "123")
 	{
 		window.location.href = "/home";
 		document.hide(document.getElementById("error"));
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@
  * GET login page.
  */
 
-// Pass in login dummy data
-var logins = require('../dummy_data/logins.json');
-
 exports.view = function(req, res){
-	res.render('index', logins);
+	res.render('index');
 };
