2c8fc11e2cfd9062c5e137472285d933ac5679a8
Kelvin <kelvin.z.pan@gmail.com>
2018-02-15 23:40:25 -0800

    Beginning of tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,27 @@
-
 /*
  * GET a tasks page.
  */
 
+var logins = require('../dummy_data/logins.json');
+var users = require('../dummy_data/users.json');
+var teams = require('../dummy_data/teams.json');
+var calendars = require('../dummy_data/calendars.json');
+var data = {
+	"logins": logins,
+	"users": users,
+	"teams": teams,
+	"calendars": calendars
+}
+
 exports.view = function(req, res){
-  res.render('tasks');
+	var teamID = req.params.teamid;
+	var team;
+	for (var i in teams.teamlist)
+	{
+		var currTeam = teams.teamlist[i];
+		if( currTeam.teamID == teamID ) team = currTeam;
+	}
+	data.currTeam = team;
+
+  res.render('tasks', data);
 };
