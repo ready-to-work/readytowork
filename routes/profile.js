2cdf85affb8fed0b083307a5156edf5a73259390
Kelvin <kelvin.z.pan@gmail.com>
2018-03-15 21:10:44 -0700

    debug heroku try 2

--- routes/profile.js
+++ routes/profile.js
@@ -51,7 +51,7 @@ exports.view = function(req, res){
 				var weekDate = new Date();
 				weekDate.setDate(todaysDate.getDate() + 7);
 				var currEndDate = new Date(currEvent.endDate.substring(0,4), currEvent.endDate.substring(5,7) - 1, currEvent.endDate.substring(8));
-				if (currEndDate < todaysDate) currEvent.past = true;
+				// if (currEndDate < todaysDate) currEvent.past = true;
 			}
 
 			// Convert dates to readable format
@@ -128,31 +128,31 @@ exports.view = function(req, res){
 		}
 	}
 
-	// // Sort events by date
-	// data.currEvents.sort(function(a, b) {
-	// 	var a_date = a.sortDate;
-	// 	var a_time = a.sortTime;
-	// 	var b_date = b.sortDate;
-	// 	var b_time = b.sortTime;
+	// Sort events by date
+	data.currEvents.sort(function(a, b) {
+		var a_date = a.sortDate;
+		var a_time = a.sortTime;
+		var b_date = b.sortDate;
+		var b_time = b.sortTime;
 
-	// 	a_date = new Date(Date.parse(a_date + " " + a_time));
-	// 	b_date = new Date(Date.parse(b_date + " " + b_time));
+		a_date = new Date(Date.parse(a_date + " " + a_time));
+		b_date = new Date(Date.parse(b_date + " " + b_time));
 
-	// 	return a_date - b_date;
-	// });
+		return a_date - b_date;
+	});
 
-	// // Sort tasks by date
-	// data.currTasks.sort(function(a, b) {
-	// 	var a_date = a.sortDate;
-	// 	var a_time = a.sortTime;
-	// 	var b_date = b.sortDate;
-	// 	var b_time = b.sortTime;
+	// Sort tasks by date
+	data.currTasks.sort(function(a, b) {
+		var a_date = a.sortDate;
+		var a_time = a.sortTime;
+		var b_date = b.sortDate;
+		var b_time = b.sortTime;
 
-	// 	a_date = new Date(Date.parse(a_date + " " + a_time));
-	// 	b_date = new Date(Date.parse(b_date + " " + b_time));
+		a_date = new Date(Date.parse(a_date + " " + a_time));
+		b_date = new Date(Date.parse(b_date + " " + b_time));
 
-	// 	return a_date - b_date;
-	// });
+		return a_date - b_date;
+	});
 	
 	res.render('profile', data);
 };
